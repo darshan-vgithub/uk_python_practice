0d13138d9033f6ba8567dc258022bdd7f8bb429a
Darshan.V <darshanv487@gmail.com>
2025-09-29 17:56:32 +0100

    learnt about how to install modules in the python

---
+++ CodeWithHarry/Chapter-1/chapter-1-practiceset/problem2.py
@@ -0,0 +1,2 @@
+# Use REPL and print the table of 5 using it.
+print("5*1=5\n5*2=10\n5*3=15\n5*4=20\n5*5=25\n5*6=30\n5*7=35\n5*8=40\n5*9=45\n5*10=50")
---
+++ CodeWithHarry/Chapter-1/chapter-1-practiceset/problem5.py
@@ -0,0 +1,11 @@
+# Label the program written in problem 4 with comments.
+
+
+# Import the 'os' module from Python's standard library
+# This module lets us interact with the operating system (like files, folders, paths).
+import os  
+
+# Use the os.listdir() function to list all files and directories 
+# in the current working directory.
+# The print() function then outputs that list to the console.
+print(os.listdir())  
