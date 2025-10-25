a07a82d3fcfb308032369554b08e5aa4b1820e67
Darshan.V <darshanv487@gmail.com>
2025-10-25 18:23:13 +0100

    chapter-2/: learnt  about the typecasting

---
+++ CodeWithHarry/chapter-2/operators.py
@@ -0,0 +1,30 @@
+a=7
+b=4
+c=a+b
+print(c)
+
+
+a=7-2
+print(a)
+
+
+b=6
+b+=3
+print(b)
+
+# comparison operators
+a=3
+b=4
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+# logical operators
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)
---
+++ CodeWithHarry/chapter-2/typecatsing.py
@@ -0,0 +1,12 @@
+# typecasting
+a=1
+b="Darshan"
+c=1.0
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+a1=int(a)
+print(type(a1))
